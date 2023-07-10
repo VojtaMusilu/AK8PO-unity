1f13d55e0880ea990bf214587a6211329b30aa38
VojtaMusilu <88598423+VojtaMusilu@users.noreply.github.com>
2023-07-10 23:52:30 +0200

    timer, different player inputs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,73 +1,40 @@
 using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     public float playerSpeed;
     private bool isMoving;
     private Vector2 input;
+    private Vector2 movement;
     private Animator animator;
-    public LayerMask solidObjectsLayer;
-
+    private Rigidbody2D rb;
     private void Awake()
     {
         animator = GetComponent<Animator>();
-        
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnMovement(InputValue value)
+    {
+        movement = value.Get<Vector2>();
     }
 
     private void Update()
     {
-        if (!isMoving)
-        {
-            input.x = Input.GetAxisRaw("Horizontal");
-            input.y = Input.GetAxisRaw("Vertical");
-            
-            
-
-            if (input.x != 0)
-            {
-                input.y = 0;
-            }
-
-            if (input != Vector2.zero)
-            {
-                animator.SetFloat("moveX", input.x);
-                animator.SetFloat("moveY", input.y);
-
-                var targetPosition = transform.position;
-                targetPosition.x += input.x;
-                targetPosition.y += input.y;
-
-                if (isWalkable(targetPosition))
-                {
-                    StartCoroutine(Move(targetPosition));
-                }
-            }
-            
-        }
+        animator.SetFloat("moveX", movement.x);
+        animator.SetFloat("moveY", movement.y);
+        isMoving = true;
+        animator.SetBool("isWalking", isMoving);
+        rb.MovePosition(rb.position + movement * Time.deltaTime * playerSpeed);
+        isMoving = false;
         animator.SetBool("isWalking", isMoving);
     }
-
-    private IEnumerator Move(Vector3 targetPosition)
+    
+    private void OnCollisionEnter2D(Collision2D other)
     {
-        isMoving = true;
-        while ((targetPosition - transform.position).sqrMagnitude > Mathf.Epsilon)
-        {
-            transform.position = Vector3.MoveTowards(
-                transform.position, targetPosition, playerSpeed*Time.deltaTime);
-            yield return null;
-        }
-        transform.position = targetPosition;
-        isMoving = false;
-    }
-
-    private bool isWalkable(Vector3 targetPosition)
-    {
-        if (Physics2D.OverlapCircle(targetPosition, 0.2f, solidObjectsLayer) != null)
-        {
-            return false;
-        }
-        return true;
+        Debug.Log("hit by enemy");
     }
 }
